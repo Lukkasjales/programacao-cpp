cd8ca804cbfd84d28bfb04c9d7c1a3cec993277a
Lucas Jales <lucasjales990@gmail.com>
2024-09-16 13:38:47 -0300

    teste

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+#include <string>
+using namespace std;
+
+struct Student {
+    string name;
+    string registration;
+    string group;
+};
+ 
+struct Discipline {
+    string name;
+    string id;
+};
+
